281984ba5a28d637568963eb3bbec48a4681499d
Leckar <gusleckar@gmail.com>
2022-09-14 15:26:33 -0300

    innitial commit

---
+++ src/pages/Login.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" data-testid="email-input" />
+        <input type="password" data-testid="password-input" />
+        <button type="submit">Entrar</button>
+      </div>
+    );
+  }
+}
+
+export default Login;
