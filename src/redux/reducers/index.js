a5aebe06978e0e2a1d0c47161c7229a3c03e0f77
Leckar <gusleckar@gmail.com>
2022-09-19 16:45:44 -0300

    setup

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,5 +1,39 @@
+import { combineReducers } from 'redux';
+import { GET_ERROR, REQUEST_API, RESPONSE_API } from '../actions';
+
 // import user from './user';
 // import wallet from './wallet';
 
 // Configure os seus reducers.
 // ATENÇÃO: você obrigatoriamente tem que utilizar as chaves "user" e "wallet" no seu estado global
+const INITIAL_STATE = {
+  loading: false,
+  error: null,
+};
+
+function getMoney(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case REQUEST_API:
+    return {
+      ...state,
+      loading: true,
+
+    };
+  case RESPONSE_API:
+    return {
+      ...state,
+      loading: false,
+    };
+  case GET_ERROR:
+    return {
+      ...state,
+      error: action.error,
+    };
+  default:
+    return state;
+  }
+}
+
+const rootReducer = combineReducers({ getMoney });
+
+export default rootReducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,1 +1,14 @@
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+import thunk from 'redux-thunk';
+import rootReducer from './reducers';
+
 // configure aqui sua store
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(
+    applyMiddleware(thunk),
+  ),
+);
+
+export default store;
